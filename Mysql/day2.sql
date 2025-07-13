1075d0127a9aeceb5d3f61d5bad63a8298ad337e
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-07-13 23:24:08 +0530

    day 2  of learning my sql

---
+++ Mysql/day2.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE google;
+USE google;
+
+CREATE TABLE employeeinfo(
+id INT PRIMARY KEY,
+name VARCHAR(30),
+salary FLOAT
+);
+
+INSERT INTO employeeinfo
+(id ,name, salary)
+values
+(1, "rahul", 100000.50),
+(2, "shubh", 1000000.50),
+(3, "shreya", 200000.50);
+
+SELECT * FROM employeeinfo;
+RENAME TABLE employeeinfo to employees;
+
+-- primary key should be unique and not null
+-- foregin key which is primary key in another table and it can be 
+-- duplicate and null values;
+
+
+CREATE TABLE temp1(
+id INT UNIQUE
+);
+
+INSERT INTO temp1 VALUES(101);
+ -- INSERT INTO temp1 VALUES(101); IT WILL GIVE ERROR
+
