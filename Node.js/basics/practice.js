9e16701648b55300d62164e08b8e4d604507226f
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-07-20 19:14:55 +0530

    practice set day 3 completed

--- Node.js/basics/practice.js
+++ Node.js/basics/practice.js
@@ -12,7 +12,21 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) =>{
-console.log(req.headers);
+ if(req.url ==='/home'){
+    res.write('<h1> welcome to home page </h1>')
+    return res.end();
+ }else if(req.url === '/men'){
+    res.write('<h1> welcome to men page </h1>') 
+    return res.end(); 
+ }else if(req.url === '/women'){
+    res.write('<h1> welcome to women page </h1>') 
+    return res.end(); 
+ }else if(req.url === '/cart'){
+    res.write('<h1> welcome to cart page </h1>') 
+    return res.end();
+ }
+
+
 res.write(
     `<head>
     <title>Nav links</title>
