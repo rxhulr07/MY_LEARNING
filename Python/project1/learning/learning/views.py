faa97199f35c859bc7416e54ee14ce4f0776b4ca
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-08-16 22:53:59 +0530

    learning templates

---
+++ Python/project1/learning/learning/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, Welcome to Home Page")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("Hello, Welcome to about Page")
+
+def contact(request):
+    return HttpResponse("Hello, Welcome to contact Page")
+
