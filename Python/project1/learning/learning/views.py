db0293c65e9bd9357ec1895b56af3841aa577949
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-08-16 23:28:38 +0530

    learned to load static files

--- Python/project1/learning/learning/views.py
+++ Python/project1/learning/learning/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello, Welcome to Home Page")
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello, Welcome to about Page")
